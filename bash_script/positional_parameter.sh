1872e6e11a85f5dd3148cfeedae6f5881c5868f1
Repfas <ramdani.fahri23@gmail.com>
2025-09-20 17:23:05 +0700

    learn bash

---
+++ bash_script/positional_parameter.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Script name: $0"
+echo "Number of arguments: $#"
+echo "First argument (Source File): $1"
+echo "Second argument (Target Dir): $2"
+
+echo "next argument is: $3 " 
---
+++ bash_script/trying_bash.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Hello, World from Bash!"
+echo "The current date and time is: $(date)"
+echo "I am user: $(whoami)"
+First_name="xxxxxx"
+Last_name='xxxxxxx'
+sentence="I'm a lord of the blood castle"
+count=1000
+echo $First_name $Last_name $sentences abcd
+echo "there're $count files"
+echo Hai. What is yourname?
+read yourname
+echo oke how do you do?
+read job
+echo "Hei $yourname, have a good $job"
